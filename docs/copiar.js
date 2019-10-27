1ea15d70bf3871a767637ff09b933c37a129795b
DibujaOtakus <dibujaotakus@gmail.com>
2019-10-27 12:00:53 +0100

    1

--- docs/copiar.js
+++
@@ -1,7 +0,0 @@
-function CopyToClipboard(element) {
-    var $temp = $("<textarea>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
